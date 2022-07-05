a5af20404a0e44d8825a79dfda7213e6426960a5
priyanshu1607 <priyanshumathur215@gmail.com>
2022-07-05 17:21:34 +0530

    complete

--- input.js
+++ input.js
@@ -1,30 +1,22 @@
-// document.getElementById("+").onclick 
-// document.getElementById("1").onclick 
-// document.getElementById("2").onclick
-// document.getElementById("3").onclick
-
-// document.getElementById("-").onclick
-// document.getElementById("4").onclick
-// document.getElementById("5").onclick
-// document.getElementById("6").onclick
-
-// document.getElementById("*").onclick
-// document.getElementById("7").onclick
-// document.getElementById("8").onclick
-// document.getElementById("9").onclick
-
-// document.getElementById("/").onclick
-// document.getElementById("%").onclick
-// document.getElementById("0").onclick
-
 document.getElementById("=").onclick = function result(){
     let str= document.getElementById("input_field")
    
-    str2= eval(str.value)
+    let str2= eval(str.value)
     str.value=str2
 
 }
 
+document.getElementById("ce").onclick = function clear(){
+    input.value= ""
+}
 
-var butt =document.getElementsByClassName("button")
+var btn =document.getElementsByClassName("button")
 var input=document.getElementById("input_field")
+
+for (let i = 0; i < btn.length; i++) 
+{
+    btn[i].onclick = function values(){
+        input.value= input.value + btn[i].innerHTML
+
+    }
+}
