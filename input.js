9a456693c21297ab7f9e68068f23251adf8c32ae
priyanshu1607 <priyanshumathur215@gmail.com>
2022-07-05 15:59:06 +0530

    hh

--- input.js
+++ input.js
@@ -1,19 +1,30 @@
-document.getElementById("+").onclick 
-document.getElementById("1").onclick 
-document.getElementById("2").onclick
-document.getElementById("3").onclick
+// document.getElementById("+").onclick 
+// document.getElementById("1").onclick 
+// document.getElementById("2").onclick
+// document.getElementById("3").onclick
 
-document.getElementById("-").onclick
-document.getElementById("4").onclick
-document.getElementById("5").onclick
-document.getElementById("6").onclick
+// document.getElementById("-").onclick
+// document.getElementById("4").onclick
+// document.getElementById("5").onclick
+// document.getElementById("6").onclick
 
-document.getElementById("*").onclick
-document.getElementById("7").onclick
-document.getElementById("8").onclick
-document.getElementById("9").onclick
+// document.getElementById("*").onclick
+// document.getElementById("7").onclick
+// document.getElementById("8").onclick
+// document.getElementById("9").onclick
 
-document.getElementById("/").onclick
-document.getElementById("%").onclick
-document.getElementById("0").onclick
-document.getElementById("=").onclick
+// document.getElementById("/").onclick
+// document.getElementById("%").onclick
+// document.getElementById("0").onclick
+
+document.getElementById("=").onclick = function result(){
+    let str= document.getElementById("input_field")
+   
+    str2= eval(str.value)
+    str.value=str2
+
+}
+
+
+var butt =document.getElementsByClassName("button")
+var input=document.getElementById("input_field")
